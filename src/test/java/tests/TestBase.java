ae52e89097c8ac5ddb8ba4d8c092edec79c4c87f
Klimova Natalia <skorpnat@gmail.com>
2024-01-12 16:15:32 +0600

    init project

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,27 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestBase {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = false;
+        Configuration.timeout = 5000; // default 4000
+
+    }
+
+    @AfterEach
+    void clearAll() {
+        clearBrowserCookies();
+        clearBrowserLocalStorage();
+        closeWebDriver();
+    }
+
+}
